e056502bb1cfb2ca6ea121b7f0865d5324e6e155
AdelinV <52411634+AdelinV@users.noreply.github.com>
2019-07-19 10:49:19 +0300

    Initial Setup

--- myapp.js
+++ myapp.js
@@ -1,3 +1,7 @@
+// if (process.env.NODE_ENV !=='production'){
+//     require('dotenv').parse()
+// }
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
